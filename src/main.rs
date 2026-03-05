7aaee438d4f976768dd46913677b032a9c54acc5
i-am-unknown-81514525 <74453352+i-am-unknown-81514525@users.noreply.github.com>
2026-03-05 23:27:13 +0000

    Add basic websocket server implementation

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,63 @@
+use futures_util::{SinkExt, StreamExt};
 mod unify;
 
-fn main() {
+use tokio;
+use axum::{
+    body::Bytes,
+    extract::ws::{Message, Utf8Bytes, WebSocket, WebSocketUpgrade},
+    response::IntoResponse,
+    routing::any,
+    Router
+};
+use axum_extra::TypedHeader;
+
+use std::ops::ControlFlow;
+use std::{net::SocketAddr, path::PathBuf};
+use axum::extract::ConnectInfo;
+use axum::extract::ws::CloseFrame;
+use tower_http::{
+    trace::{DefaultMakeSpan, TraceLayer},
+};
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
+
+#[tokio::main]
+async fn main() {
     println!("Hello, world!");
+    tracing_subscriber::registry()
+        .with(
+            tracing_subscriber::EnvFilter::try_from_default_env().unwrap_or_else(|_| {
+                format!("{}=debug,tower_http=debug", env!("CARGO_CRATE_NAME")).into()
+            }),
+        )
+        .with(tracing_subscriber::fmt::layer())
+        .init();
+    let app = Router::new()
+        .route("/ws", any(news_ws_handler))
+        .layer(
+            TraceLayer::new_for_http()
+                .make_span_with(DefaultMakeSpan::default().include_headers(true)),
+        );
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
+        .await
+        .unwrap();
+    tracing::debug!("listening on {}", listener.local_addr().unwrap());
+    let _ = axum::serve(
+        listener,
+        app.into_make_service_with_connect_info::<SocketAddr>(),
+    )
+        .await;
+}
+
+async fn news_ws_handler(
+    ws: WebSocketUpgrade,
+    user_agent: Option<TypedHeader<headers::UserAgent>>,
+    ConnectInfo(addr): ConnectInfo<SocketAddr>,
+) -> impl IntoResponse {
+    ws.on_upgrade(move |socket| async move {
+        let (mut sender, mut receiver) = socket.split();
+        sender.send(Message::Close(Some(CloseFrame {
+            code: axum::extract::ws::close_code::NORMAL,
+            reason: Default::default(),
+        }))).await.ok();
+    })
 }
