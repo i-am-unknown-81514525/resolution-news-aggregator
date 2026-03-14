881317ff4112835c9a2356407227ad7f2ed7f76c
i-am-unknown-81514525 <74453352+i-am-unknown-81514525@users.noreply.github.com>
2026-03-14 19:48:36 +0000

    Add request fetching

---
+++ src/plugins/rss_fetch.rs
@@ -0,0 +1,25 @@
+use reqwest;
+use once_cell::sync::Lazy;
+use serde::{Deserialize, Serialize};
+use thiserror::Error;
+use serde_xml_rs::{from_str};
+
+#[derive(Error, Debug)]
+pub(crate) enum RssFetchError {
+    RequestError(#[from] reqwest::Error),
+    SerdeParseError()
+}
+
+static CLIENT: Lazy<reqwest::Client> = Lazy::new(|| {
+    reqwest::Client::new()
+});
+
+async fn get_raw(url: reqwest::Url) -> Result<String, RssFetchError> {
+    CLIENT.get(url).send().await?.text().await.map_err(|e| RssFetchError::RequestError(e))
+}
+
+async fn fetch_rss<T: Deserialize>(url: reqwest::Url) -> Result<T, RssFetchError> {
+    let response = get_raw(url).await?;
+    let parsed: T = from_str(&response);
+    Ok(parsed)
+}
