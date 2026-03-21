5f87517fa662ed71ea672d5df74903a76740dc63
i-am-unknown-81514525 <74453352+i-am-unknown-81514525@users.noreply.github.com>
2026-03-21 21:03:26 +0000

    Add post_process API

--- server/src/plugins/source/mod.rs
+++ server/src/plugins/source/mod.rs
@@ -4,12 +4,13 @@ mod hacker_news;
 pub(crate) use crate::plugins::net::rss_fetch::RssFetchError;
 use crate::plugins::source::google_rss_search::GoogleRssSearch;
 use crate::value_enum::{EnumFromStr, value_enum};
-use common::unify::ToVecUnify;
+use common::unify::{ToVecUnify, UnifyOutput};
 use serde::Deserialize;
 use serde_xml_rs::from_str;
 
 value_enum!(RSSSourceType, DirectRss, GoogleWrap, GoogleRssSearch);
 
+#[async_trait::async_trait]
 pub(crate) trait RSSSource: Send + Sync {
     type Deserialize<'a>: Deserialize<'a> + ToVecUnify + Send + Sync
     where
@@ -18,6 +19,10 @@ pub(crate) trait RSSSource: Send + Sync {
     fn deserialize(&self, content: &str) -> Result<Self::Deserialize<'_>, RssFetchError> {
         let parsed: Self::Deserialize = from_str(content).map_err(|e| RssFetchError::SerdeXmlParseError(e))?;
         Ok(parsed)
+    }
+
+    async fn post_process(&self, content: Vec<UnifyOutput>) -> Vec<UnifyOutput> {
+        content
     }
 }
 
